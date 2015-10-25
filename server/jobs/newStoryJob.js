cab67e0854e380cca91fe7f5c066671b224368ff
Mitchell Wulfman <mitchwulfman@gmail.com>
2015-10-25 15:45:02 -0400

    add dummy trend data until we get the API back

--- server/jobs/newStoryJob.js
+++ server/jobs/newStoryJob.js
@@ -17,12 +17,12 @@ let createTweetJob = function () {
     .priority('normal')
     .retry({
       retries: 5,                         // If fail, retry 5 times
-      wait: 60*60*1000                     // half a minute between attempts
+      wait: 1000                     // half a minute between attempts
     })
     .repeat({
       //repeats: tweetJobs.forever,  // repeats forever
       //wait: 60 * 60 * 1000                // hour between repeats
-      wait: 60*60*1000
+      wait: 1000
     })
     .save();                               // Submit job to the queue
 };
@@ -43,7 +43,7 @@ tweetJobs.processJobs(
   {
     concurrency: 1,      //max number of simultaneous outstanding async calls to worker allowed
     payload: 1,             //max number of job objects to provide to each worker
-    pollInterval: 60*60*1000,   //how often to check the collection for more
+    pollInterval: 1000,   //how often to check the collection for more
     // jobs (ms)
     prefetch: 0           //how many extra jobs to request to compensate for latency
   },
@@ -53,12 +53,74 @@ tweetJobs.processJobs(
       T.get('trends/place', {
         id: 23424977
       }, (err, data, response) => {
-        console.log(data)
+        //console.log(data)
         console.log(err)
-        console.log(response)
+        //console.log(response)
 
-        let trends = data[0].trends
-        logEach(trends);
+        //let trends = data[0].trends
+        //logEach(trends);
+        let trends = [
+          {
+            name: 'Flip Saunders',
+            query: '%22Flip+Saunders%22',
+            url: 'http://twitter.com/search?q=%22Flip+Saunders%22',
+            promoted_content: null
+          },
+          {
+            name: 'Texans',
+            query: 'Texans',
+            url: 'http://twitter.com/search?q=Texans',
+            promoted_content: null
+          },
+          {
+            name: 'Andrew Luck',
+            query: '%22Andrew+Luck%22',
+            url: 'http://twitter.com/search?q=%22Andrew+Luck%22',
+            promoted_content: null
+          },
+          {
+            name: 'Jets',
+            query: 'Jets',
+            url: 'http://twitter.com/search?q=Jets',
+            promoted_content: null
+          },
+          {
+            name: '#BlueNeighbourhood',
+            query: '%23BlueNeighbourhood',
+            url: 'http://twitter.com/search?q=%23BlueNeighbourhood',
+            promoted_content: null
+          },
+          {
+            name: '#Saints',
+            query: '%23Saints',
+            url: 'http://twitter.com/search?q=%23Saints',
+            promoted_content: null
+          },
+          {
+            name: '#Redskins',
+            query: '%23Redskins',
+            url: 'http://twitter.com/search?q=%23Redskins',
+            promoted_content: null
+          },
+          {
+            name: '#WhoDat',
+            query: '%23WhoDat',
+            url: 'http://twitter.com/search?q=%23WhoDat',
+            promoted_content: null
+          },
+          {
+            name: 'Blac Chyna',
+            query: '%22Blac+Chyna%22',
+            url: 'http://twitter.com/search?q=%22Blac+Chyna%22',
+            promoted_content: null
+          },
+          {
+            name: 'Jarvis Landry',
+            query: '%22Jarvis+Landry%22',
+            url: 'http://twitter.com/search?q=%22Jarvis+Landry%22',
+            promoted_content: null
+          }
+        ]
 
         R.forEach(boundInsert, trends)
       });
